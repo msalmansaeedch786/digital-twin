2383d859ea19d2622cb6b047d0aa5bae926d07eb
s1salmmu <muhammad.salman@thoughtworks.com>
2026-06-26 01:49:56 +0200

    security: revert AdministratorAccess and use scoped least privilege policy

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -80,7 +80,10 @@ resource "aws_iam_policy" "github_actions_policy" {
           "secretsmanager:*",
           "cloudtrail:*",
           "sns:*",
-          "cloudwatch:*"
+          "cloudwatch:*",
+          "kms:*",
+          "xray:*",
+          "bedrock:*"
         ]
         Resource = "*"
       }
@@ -90,7 +93,7 @@ resource "aws_iam_policy" "github_actions_policy" {
 
 resource "aws_iam_role_policy_attachment" "github_actions" {
   role       = aws_iam_role.github_actions.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = aws_iam_policy.github_actions_policy.arn
 }
 
 output "github_actions_role_arn" {
