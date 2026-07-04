6af329f8fe32c6a4762beb8b6b1a633f2d533a92
Muhammad Salman <msalmansaeedch786@gmail.com>
2026-07-04 03:39:05 +0200

    fix(terraform): format frontend_url correctly and remove arn output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,5 +4,5 @@
 
 output "frontend_url" {
   description = "The public URL for the Digital Twin Frontend Application"
-  value       = "https://${aws_amplify_branch.feature.branch_name}.${aws_amplify_app.frontend.default_domain}"
+  value       = "https://${replace(aws_amplify_branch.feature.branch_name, "/", "-")}.${aws_amplify_app.frontend.default_domain}"
 }
